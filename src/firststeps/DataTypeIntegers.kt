7efba120388f57c2949898425e1cbcce83c77588
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2024-04-08 14:19:58 -0300

    Integers

---
+++ src/firststeps/DataTypeIntegers.kt
@@ -0,0 +1,16 @@
+package firststeps
+
+fun main() {
+    /**
+     * Byte: 1 byte - 8 bits
+     * Short: 2 bytes - 16 bits
+     * => Int: 4 bytes - 32 bits
+     * => Long: 8 bytes - 64 bits
+     */
+
+    val i1 = 10
+    val i2: Int = 10
+    val i3: Int
+
+    val l1 = 10L
+}
