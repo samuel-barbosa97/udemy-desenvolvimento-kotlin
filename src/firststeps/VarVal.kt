97b65adbc54a88afd6bd9407f0e71807f0a1a5f9
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2024-04-08 14:14:03 -0300

    Var e Val

--- src/firststeps/VarVal.kt
+++ src/firststeps/VarVal.kt
@@ -2,4 +2,15 @@ package firststeps
 
 fun main() {
 
+//    var counter: Int
+//    counter = 10
+
+//    var counter: Int = 10
+
+    var counter = 10
+    counter = 5
+
+    val counter2 = 10
+
+
 }
