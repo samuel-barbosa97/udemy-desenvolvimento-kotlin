5dee610d719452b3f810b335b1e1f2a95b1314ef
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2024-04-12 14:33:34 -0300

    Type String

---
+++ src/firststeps/DataTypeString.kt
@@ -0,0 +1,11 @@
+package firststeps
+
+fun main() {
+
+    val s1 = "abc"
+    val s2 = "def"
+
+    //val s1s2 = "Soma: " + (s1 + s2)
+    val s1s2 = "Soma: $s1$s2. O tamanho é ${(s1+s2).length}"
+    println(s1s2)
+}
