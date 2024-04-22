c42854f77184a4ed1399bf92d405fd53d9bdf9f7
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-22 09:19:46 -0300

    Data TYpes Literals pt01

---
+++ src/firststeps/DataTypeStringLiterals.kt
@@ -0,0 +1,12 @@
+package firststeps
+
+fun main() {
+    /*
+    O rato roeu
+    a roupa do
+    rei de "Roma"
+     */
+
+    val s1 = "O rato roeu\na roupa do\nrei de \"Roma\"."
+    print(s1)
+}
