ed5e124289de9f2311a3b582be7bd144e77531d9
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-23 18:43:14 -0300

    Exercice 2

---
+++ src/firststeps/ExRound.kt
@@ -0,0 +1,14 @@
+package firststeps
+
+/**
+ * EXERCÍCIO
+ * ---------
+ *
+ * Crie um porgrama que arredonda um valor decimal informado pelo usuário e mostra a resposta na tela.
+ * O arredondamento pode ser feito truncando as casas decímais.
+ */
+fun main() {
+
+    print("> ")
+    println(readln().toDouble().toInt())
+}
