b00ac08e66d5aded8f0e02e247b379e73888eee7
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-23 18:29:58 -0300

    Scanner

---
+++ src/firststeps/KeyboardInputScanner.kt
@@ -0,0 +1,23 @@
+package firststeps
+
+import java.util.Scanner
+
+fun main() {
+
+//    print("> ")
+//    val n1 = readln().toInt()
+//
+//    print("> ")
+//    val n2 = readln().toInt()
+//
+//    println("Result: ${n1+n2}")
+
+    val sc = Scanner(System.`in`)
+    print("> ")
+    val n1 = sc.nextInt()
+
+    print("> ")
+    val n2 = sc.nextInt()
+
+    println("Result: ${n1+n2}")
+}
