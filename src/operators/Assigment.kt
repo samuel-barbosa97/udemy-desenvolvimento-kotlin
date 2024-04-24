5bb69ed1a39eb3171c3067f8df99c390d6fc3947
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-24 11:07:18 -0300

    Assigment operators

---
+++ src/operators/Assigment.kt
@@ -0,0 +1,24 @@
+package operators
+
+fun main() {
+
+    var x = 5
+
+    x += 2 // x = x + 2
+    println(x)
+
+    x *= 2 // x = x * 2
+    println(x)
+
+    x -= 2 // x = x * 2
+    println(x)
+
+    x /= 3 // x = x * 2
+    println(x)
+
+    x++
+    println(x)
+
+    x--
+    println(x)
+}
