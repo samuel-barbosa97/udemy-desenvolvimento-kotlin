05c76d75c9ab26a8e8010980625d403ebcd880f7
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-26 16:54:14 -0300

    If Else If

---
+++ src/selection/IfElseIf.kt
@@ -0,0 +1,14 @@
+package selection
+
+fun main() {
+
+    val v = 10
+
+    if (v > 0) {
+        println("Positivo")
+    } else if (v < 0) {
+        println("Negativo")
+    } else {
+        println("Zero")
+    }
+}
