07da4a25c72512bea6ccf32fdede040f7680b94a
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-30 14:34:34 -0300

    Do While

---
+++ src/repetition/DoWhile.kt
@@ -0,0 +1,29 @@
+package repetition
+
+fun main() {
+
+//    var x = 0
+//
+//    while (x <= 10) {
+//        print("$x ")
+//        x++
+//    }
+//
+//    println()
+//    x = 0
+//
+//    do {
+//        print("$x ")
+//        x++
+//    } while (x <= 10)
+
+    var x = 10
+
+    while (x < 5) {
+        print("$x ")
+    }
+
+    do {
+        print("$x ")
+    } while (x < 5)
+}
