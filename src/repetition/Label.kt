05891e374ca06d2bec0463e468931fe643809ed4
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-30 15:24:54 -0300

    Label

---
+++ src/repetition/Label.kt
@@ -0,0 +1,17 @@
+package repetition
+
+fun main() {
+
+    loop@ for (i in 'A'..'E') {
+        for (j in 1..5) {
+            print("$i$j ")
+
+            if (j == 3) {
+//                break@loop
+                continue@loop
+            }
+        }
+        println()
+    }
+    println("\nFIM")
+}
