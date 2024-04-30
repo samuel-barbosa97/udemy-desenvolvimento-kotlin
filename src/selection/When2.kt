3dcf036972abcc552665d176e045fb03922b8377
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-30 10:55:14 -0300

    When 2

---
+++ src/selection/When2.kt
@@ -0,0 +1,12 @@
+package selection
+
+fun main() {
+
+    val x = 0
+
+    when {
+        x > 0 -> println("Positivo")
+        x < 0 -> println("Negativo")
+        else -> println("ZERO")
+    }
+}
