624523bf4aab85ed81a8c6e223fe17ffc41182da
Samuel Barbosa <tqi_sbarbosa@uolinc.com>
2024-04-30 10:44:13 -0300

    If Expression

---
+++ src/selection/IfExpression.kt
@@ -0,0 +1,15 @@
+package selection
+
+fun main() {
+
+    val number = 10
+
+    val result = if (number % 2 == 0) {
+        println("PAR")
+        "PAR"
+    } else {
+        println("PAR")
+        "PAR"
+    }
+    println(result)
+}
